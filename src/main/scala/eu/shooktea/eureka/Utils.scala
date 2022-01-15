735df0212cc7bd81a2b3bec834120e32a81b856a
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-01-15 22:13:51 +0100

    Calculating least common multiple

--- src/main/scala/eu/shooktea/eureka/Utils.scala
+++ src/main/scala/eu/shooktea/eureka/Utils.scala
@@ -7,4 +7,8 @@ object Utils {
   def greatestCommonDivisor(a: Int, b: Int): Int =
     if (b == 0) a
     else greatestCommonDivisor(b, a % b)
+
+  def leastCommonMultiple(a: Int, b: Int): Int =
+    if (a == 0 && b == 0) 0
+    else Math.abs(a * b) / greatestCommonDivisor(a, b)
 }
--- src/test/scala/eu/shooktea/eureka/UtilsTest.scala
+++ src/test/scala/eu/shooktea/eureka/UtilsTest.scala
@@ -7,4 +7,9 @@ class UtilsTest extends AnyFunSuite {
     assert(Utils.greatestCommonDivisor(5, 1) == 1)
     assert(Utils.greatestCommonDivisor(4, 6) == 2)
   }
+
+  test("Least common multiple") {
+    assert(Utils.leastCommonMultiple(3, 5) == 15)
+    assert(Utils.leastCommonMultiple(6, 8) == 24)
+  }
 }
