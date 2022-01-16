ce1f02b924e9928ace7d6687275d656e9e3f57c0
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-01-16 12:17:36 +0100

    More ways of creating decimal number

--- src/main/scala/eu/shooktea/eureka/constant/DecimalNumber.scala
+++ src/main/scala/eu/shooktea/eureka/constant/DecimalNumber.scala
@@ -6,4 +6,8 @@ class DecimalNumber private(val decimal: BigDecimal) extends Constant {
 
 object DecimalNumber {
   def apply(decimal: BigDecimal): DecimalNumber = new DecimalNumber(decimal)
+  def apply(int: BigInt): DecimalNumber = DecimalNumber(BigDecimal(int))
+  def apply(double: Double): DecimalNumber = DecimalNumber(BigDecimal(double))
+  def apply(long: Long): DecimalNumber = DecimalNumber(BigInt(long))
+  def apply(string: String): DecimalNumber = DecimalNumber(BigDecimal(string))
 }
--- src/test/scala/eu/shooktea/eureka/constant/DecimalNumberTest.scala
+++ src/test/scala/eu/shooktea/eureka/constant/DecimalNumberTest.scala
@@ -4,6 +4,37 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class DecimalNumberTest extends AnyFunSuite {
   test("Creating from big decimal") {
-    assert(DecimalNumber(BigDecimal(-15.25)).toBigDecimal == BigDecimal(-15.25))
+    val decimal = DecimalNumber(BigDecimal(-15.25))
+    assert(decimal.decimal == BigDecimal(-15.25))
+    assert(decimal.toBigDecimal == BigDecimal(-15.25))
+    assert(decimal.roundToBigInt == BigInt(-15))
+  }
+
+  test("Creating from big integer") {
+    val decimal = DecimalNumber(BigInt(13))
+    assert(decimal.decimal == BigDecimal(13))
+    assert(decimal.toBigDecimal == BigDecimal(13))
+    assert(decimal.roundToBigInt == BigInt(13))
+  }
+
+  test("Creating from double") {
+    val decimal = DecimalNumber(-15.75)
+    assert(decimal.decimal == BigDecimal(-15.75))
+    assert(decimal.toBigDecimal == BigDecimal(-15.75))
+    assert(decimal.roundToBigInt == BigInt(-16))
+  }
+
+  test("Creating from long") {
+    val decimal = DecimalNumber(Long.MaxValue)
+    assert(decimal.decimal == BigDecimal(Long.MaxValue))
+    assert(decimal.toBigDecimal == BigDecimal(Long.MaxValue))
+    assert(decimal.roundToBigInt == BigInt(Long.MaxValue))
+  }
+
+  test("Creating from string") {
+    val decimal = DecimalNumber("0.3")
+    assert(decimal.decimal == BigDecimal("0.3"))
+    assert(decimal.toBigDecimal == BigDecimal("0.3"))
+    assert(decimal.roundToBigInt == BigInt(0))
   }
 }
