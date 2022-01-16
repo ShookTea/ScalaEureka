f2403891900af61004f2004b698c959290a5f9a9
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-01-16 13:00:50 +0100

    Removed old Fraction class

--- src/main/scala/eu/shooktea/eureka/TypeExtensions.scala
+++
@@ -1,9 +0,0 @@
-package eu.shooktea.eureka
-
-import eu.shooktea.eureka.constant.Fraction
-
-object TypeExtensions {
-  implicit class IntExtension(val i: Int) {
-    def +(f: Fraction): Fraction = f + i
-  }
-}
