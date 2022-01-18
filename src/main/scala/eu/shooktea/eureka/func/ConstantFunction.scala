460c9cd8d2687b197a7f4bb778a1ccd19be9008a
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-01-18 13:42:39 +0100

    Better ConstantFunction

--- src/main/scala/eu/shooktea/eureka/func/ConstantFunction.scala
+++ src/main/scala/eu/shooktea/eureka/func/ConstantFunction.scala
@@ -2,12 +2,13 @@ package eu.shooktea.eureka.func
 
 import eu.shooktea.eureka.constant.{Constant, DecimalNumber}
 
-class ConstantFunction(val constant: Constant) extends Function {
+class ConstantFunction(val constant: BigDecimal) extends Function {
   override def toString: String = constant.toString
-  override def apply(args: Map[Variable, BigDecimal]): BigDecimal = constant.toBigDecimal
+  override def apply(args: Map[Variable, BigDecimal]): BigDecimal = constant
   override def derivative(v: Variable): Function = ConstantFunction(DecimalNumber(0))
 }
 
 object ConstantFunction {
-  def apply(constant: Constant): ConstantFunction = new ConstantFunction(constant)
+  def apply(constant: Constant): ConstantFunction = new ConstantFunction(constant.toBigDecimal)
+  def apply(constant: BigDecimal): ConstantFunction = new ConstantFunction(constant)
 }
--- src/main/scala/eu/shooktea/eureka/func/Variable.scala
+++ src/main/scala/eu/shooktea/eureka/func/Variable.scala
@@ -1,11 +1,10 @@
 package eu.shooktea.eureka.func
-import eu.shooktea.eureka.constant.DecimalNumber
 
 class Variable extends Function {
   override def apply(args: Map[Variable, BigDecimal]): BigDecimal = args(this)
   override def derivative(v: Variable): Function =
-    if (v == this) DecimalNumber(1).toConstFunction
-    else DecimalNumber(0).toConstFunction
+    if (v == this) ConstantFunction(BigDecimal(1))
+    else ConstantFunction(BigDecimal(0))
 }
 
 object Variable {
