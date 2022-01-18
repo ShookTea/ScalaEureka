ba8ebae6004be896e4f57a9245ae746f39b6acbf
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-01-18 16:20:37 +0100

    Cleaning functions

--- src/test/scala/eu/shooktea/eureka/func/ConstantFunctionTest.scala
+++ src/test/scala/eu/shooktea/eureka/func/ConstantFunctionTest.scala
@@ -4,8 +4,7 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class ConstantFunctionTest extends AnyFunSuite {
   test("Constant function returns itself") {
-    val const = BigDecimal(15)
-    val func = ConstantFunction(const)
-    assert(func() == const)
+    val func = ConstantFunction(15)
+    assert(func() == 15)
   }
 }
--- src/test/scala/eu/shooktea/eureka/func/VariableTest.scala
+++ src/test/scala/eu/shooktea/eureka/func/VariableTest.scala
@@ -1,13 +1,10 @@
 package eu.shooktea.eureka.func
 
-import eu.shooktea.eureka.constant.DecimalNumber
 import org.scalatest.funsuite.AnyFunSuite
 
 class VariableTest extends AnyFunSuite {
   test("Applying value to a single variable returns that value") {
-    val variable = Variable()
-    val input = BigDecimal(3.4)
-    val output = variable((variable, input))
-    assert(DecimalNumber(input) == output)
+    val v = Variable()
+    assert(v(v -> 3.4) == 3.4)
   }
 }
