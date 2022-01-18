1e851fc8c2b5a549d62c0e95178e3358c438c32e
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-01-18 13:33:44 +0100

    Remove usages of IntegerNumber from function package

--- src/main/scala/eu/shooktea/eureka/func/ConstantFunction.scala
+++ src/main/scala/eu/shooktea/eureka/func/ConstantFunction.scala
@@ -1,11 +1,11 @@
 package eu.shooktea.eureka.func
 
-import eu.shooktea.eureka.constant.{Constant, IntegerNumber}
+import eu.shooktea.eureka.constant.{Constant, DecimalNumber}
 
 class ConstantFunction(val constant: Constant) extends Function {
   override def toString: String = constant.toString
   override def apply(args: Map[Variable, Constant]): Constant = constant
-  override def derivative(v: Variable): Function = IntegerNumber(0).toConstFunction
+  override def derivative(v: Variable): Function = ConstantFunction(DecimalNumber(0))
 }
 
 object ConstantFunction {
--- src/main/scala/eu/shooktea/eureka/func/Variable.scala
+++ src/main/scala/eu/shooktea/eureka/func/Variable.scala
@@ -1,11 +1,11 @@
 package eu.shooktea.eureka.func
-import eu.shooktea.eureka.constant.{Constant, IntegerNumber}
+import eu.shooktea.eureka.constant.{Constant, DecimalNumber}
 
 class Variable extends Function {
   override def apply(args: Map[Variable, Constant]): Constant = args(this)
   override def derivative(v: Variable): Function =
-    if (v == this) IntegerNumber(1).toConstFunction
-    else IntegerNumber(0).toConstFunction
+    if (v == this) DecimalNumber(1).toConstFunction
+    else DecimalNumber(0).toConstFunction
 }
 
 object Variable {
