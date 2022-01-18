c84f965ea94a69f178e8d2af68d83cdf468af1f2
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-01-18 13:35:10 +0100

    Cleaning up IntegerNumber

--- src/main/scala/eu/shooktea/eureka/constant/IntegerNumber.scala
+++ src/main/scala/eu/shooktea/eureka/constant/IntegerNumber.scala
@@ -1,26 +1,3 @@
 package eu.shooktea.eureka.constant
 
-import eu.shooktea.eureka.extension.AllExtensions._
-
-abstract class IntegerNumber private(val integer: BigInt) extends Constant {
-  override def toBigDecimal: BigDecimal = BigDecimal(integer)
-  override def roundToBigInt: BigInt = integer
-  override def toDecimalNumber: DecimalNumber = DecimalNumber(toBigDecimal)
-
-  override def hashCode(): Int = integer.hashCode()
-  override def toString: String = integer.toString()
-  override def equals(obj: Any): Boolean = obj match {
-    case DecimalNumber(d) => d.toBigIntExact contains integer
-    case i: BigInt => i == integer
-    case l: Long => toDecimalNumber == l.toDecimalNumber
-    case i: Int => toDecimalNumber == i.toDecimalNumber
-    case d: BigDecimal => toDecimalNumber.decimal == d.toDecimalNumber.decimal
-    case d: Double => toDecimalNumber.decimal == d.toDecimalNumber.decimal
-    case _ => false
-  }
-
-  override def greaterThan(other: Constant): Boolean =
-    this.toDecimalNumber > other.toDecimalNumber
-}
-
-object IntegerNumber
+abstract class IntegerNumber private(val integer: BigInt) extends Constant
