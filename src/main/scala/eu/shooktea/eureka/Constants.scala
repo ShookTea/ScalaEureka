37da63496ac7ce3925d732b40bc663e044d57279
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-01-21 13:04:59 +0100

    Docs updated

--- src/main/scala/eu/shooktea/eureka/Constants.scala
+++ src/main/scala/eu/shooktea/eureka/Constants.scala
@@ -4,5 +4,5 @@ import eu.shooktea.eureka.func.Function._
 
 object Constants {
   val E: BigDecimal = exp(1)
-  val PI: BigDecimal = BigDecimal(Math.PI)
+  val PI: BigDecimal = BigDecimal("3.1415926535897932384626433832795029")
 }
